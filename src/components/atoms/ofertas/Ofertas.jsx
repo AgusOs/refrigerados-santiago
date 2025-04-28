e01d3bb86895e815012cabac91c2c82bdf54e713
AgusOs <osorioagus96@gmail.com>
2025-04-28 10:15:06 -0300

    carousel added

--- src/components/atoms/ofertas/Ofertas.jsx
+++ src/components/atoms/ofertas/Ofertas.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from './Ofertas.module.css'
+import { Carousel } from '../carousel/Carousel'
 
 export const Ofertas = () => {
   return (
     <div className={styles.ofertasContainer}>
       <img className={styles.ofertasSign} src='src/assets/images/ofertas-sign.png' alt='ofertas-sign'/>
-      <p>Carousel</p>
+      <Carousel/>
     </div>
   )
 }
