8e96dd6372934874c24be9c302f70285a98203f7
AgusOs <osorioagus96@gmail.com>
2025-05-05 08:53:25 -0300

    Agregando animacion slide a banner

--- src/components/atoms/banner/Banner.jsx
+++ src/components/atoms/banner/Banner.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import styles from './Banner.module.css'
+import { useInView } from 'react-intersection-observer';
+import Slide from '@mui/material/Slide';
 
 export const Banner = ({img}) => {
+  const { ref, inView } = useInView({
+    threshold: 0.3,
+    triggerOnce: true,
+  });
   return (
-    <img className={styles.banner} src={img} alt="banner" />
+    <div ref={ref}>
+    <Slide direction='left' in={inView} timeout={500}>
+      <img className={styles.banner} src={img} alt="banner" />
+      </Slide>
+    </div>
   )
 }
--- src/components/atoms/ofertas/Ofertas.jsx
+++ src/components/atoms/ofertas/Ofertas.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import styles from './Ofertas.module.css'
 import { Carousel } from '../carousel/Carousel'
+import Zoom from '@mui/material/Zoom';
+import { useInView } from 'react-intersection-observer';
 
 export const Ofertas = () => {
+  const { ref, inView } = useInView({
+    threshold: 0.3,
+    triggerOnce: true,
+  });
   return (
-    <div className={styles.ofertasContainer}>
-      <img className={styles.ofertasSign} src='src/assets/images/ofertas-sign.png' alt='ofertas-sign'/>
+    <div ref={ref} className={styles.ofertasContainer}>
+      <Zoom in={inView} timeout={500}>
+        <img className={styles.ofertasSign} src='src/assets/images/ofertas-sign.png' alt='ofertas-sign'/>
+      </Zoom>
       <Carousel/>
     </div>
   )
