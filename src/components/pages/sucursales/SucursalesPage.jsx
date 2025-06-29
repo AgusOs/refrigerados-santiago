a71abeb7d338763cf0a7ea4c4c76e6ac3b404264
AgusOs <osorioagus96@gmail.com>
2025-06-29 10:13:24 -0300

    agregando imagen sucursales

--- src/components/pages/sucursales/SucursalesPage.jsx
+++ src/components/pages/sucursales/SucursalesPage.jsx
@@ -7,9 +7,14 @@ export const SucursalesPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.portada}>
-        <Slide in={true} direction="left" timeout={1000} mountOnEnter unmountOnExit>
-          <img src="\assets\images\phone-sign.webp" alt="Portada"/>
-        </Slide>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '2rem' }}>
+          <Slide in={true} direction="left" timeout={1000} mountOnEnter unmountOnExit>
+            <img src="\assets\images\phone-sign.webp" alt="Portada"/>
+          </Slide>
+          <Slide in={true} direction="left" timeout={1000} mountOnEnter unmountOnExit>
+            <img src="\assets\images\sucursales-guy.png" alt="img" style={{width: '80%', borderRadius: '10px'}}/>
+          </Slide>
+        </div>
         <Mapa/>
       </div>
     </div>
