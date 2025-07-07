673350c074a25b4936a9bae25b42ebde5cd5f23d
AgusOs <osorioagus96@gmail.com>
2025-07-06 21:06:19 -0300

    Actualizando ofertas

--- src/components/atoms/carousel/Carousel.jsx
+++ src/components/atoms/carousel/Carousel.jsx
@@ -23,27 +23,27 @@ export const Carousel = () => {
     <Swiper {...config} className={style.swiper}>
       <SwiperSlide className={style.swiperSlide}>
         <div className={style.title}>
-          <p>MILKAUT CREMA DOBLE 200cc</p>
+          <p>LECHE LA SUIPACHENSE</p>
         </div>
-        <img src="/assets/ofertas/1.webp" alt="oferta1" />
+        <img src="/assets/ofertas/1.png" alt="oferta1" />
       </SwiperSlide>
       <SwiperSlide className={style.swiperSlide}>
         <div className={style.title}>
           <p>DULCE DE LECHE MILKAUT 400gr</p>
         </div>
-        <img src="/assets/ofertas/2.webp" alt="oferta2" />
+        <img src="/assets/ofertas/2.png" alt="oferta2" />
       </SwiperSlide>
       <SwiperSlide className={style.swiperSlide}>
         <div className={style.title}>
           <p>SALCHICHAS FELA x6u</p>
         </div>
-        <img src="/assets/ofertas/3.webp" alt="oferta3" />
+        <img src="/assets/ofertas/3.png" alt="oferta3" />
       </SwiperSlide>
       <SwiperSlide className={style.swiperSlide}>
         <div className={style.title}>
-          <p>VINO TORO CLÁSICO</p>
+          <p>TORTILLAS TIA ROSA x10u</p>
         </div>
-        <img src="/assets/ofertas/4.webp" alt="oferta4" />
+        <img src="/assets/ofertas/4.png" alt="oferta4" />
       </SwiperSlide>
       <SwiperSlide className={style.swiperSlide}>
         <div className={style.title}>
@@ -53,15 +53,9 @@ export const Carousel = () => {
       </SwiperSlide>
       <SwiperSlide className={style.swiperSlide}>
         <div className={style.title}>
-          <p>GASEOSA MANAOS 2,25Lts</p>
+          <p>QUESO UNTABLE RICREM CLÁSICO</p>
         </div>
-        <img src="/assets/ofertas/6.webp" alt="oferta6" />
-      </SwiperSlide>
-      <SwiperSlide className={style.swiperSlide}>
-        <div className={style.title}>
-          <p>DETERGENTE HÉROE 750ml</p>
-        </div>
-        <img src="/assets/ofertas/7.webp" alt="oferta6" />
+        <img src="/assets/ofertas/6.png" alt="oferta6" />
       </SwiperSlide>
       {isDesktop && (
         <>
