a84669ca318f116a35126c96d979db3352e364b1
AgusOs <osorioagus96@gmail.com>
2025-07-21 08:36:58 -0300

    Actualizando ofertas

--- src/components/atoms/carousel/Carousel.jsx
+++ src/components/atoms/carousel/Carousel.jsx
@@ -23,39 +23,33 @@ export const Carousel = () => {
     <Swiper {...config} className={style.swiper}>
       <SwiperSlide className={style.swiperSlide}>
         <div className={style.title}>
-          <p>MORTADELA PALADINI x Kg</p>
+          <p>JABON PARA ROPA HEROE 800ml</p>
         </div>
         <img src="/assets/ofertas/1.png" alt="oferta1" />
       </SwiperSlide>
       <SwiperSlide className={style.swiperSlide}>
         <div className={style.title}>
-          <p>YERBA AMANDA 500gr</p>
+          <p>YOGURT BEBIBLE MILKAUT</p>
         </div>
         <img src="/assets/ofertas/2.png" alt="oferta2" />
       </SwiperSlide>
       <SwiperSlide className={style.swiperSlide}>
         <div className={style.title}>
-          <p>SALCHICHAS FELA x6u</p>
+          <p>LECHE LARGA VIDA FRANSAFE</p>
         </div>
         <img src="/assets/ofertas/3.png" alt="oferta3" />
       </SwiperSlide>
       <SwiperSlide className={style.swiperSlide}>
         <div className={style.title}>
-          <p>LECHE LARGA VIDA FRANSAFE</p>
+          <p>FIAMBRE DE POLLO DON ALEJANDRO</p>
         </div>
         <img src="/assets/ofertas/4.png" alt="oferta4" />
       </SwiperSlide>
       <SwiperSlide className={style.swiperSlide}>
         <div className={style.title}>
-          <p>TOMATE TRITURADO DULCOR</p>
+          <p>QUESO UNTABLE MILKAUAT 285gr</p>
         </div>
-        <img src="/assets/ofertas/5.png" alt="oferta6" />
-      </SwiperSlide>
-      <SwiperSlide className={style.swiperSlide}>
-        <div className={style.title}>
-          <p>HARINA TASSARA</p>
-        </div>
-        <img src="/assets/ofertas/6.png" alt="oferta6" />
+        <img src="/assets/ofertas/5.png" alt="oferta5" />
       </SwiperSlide>
       <SwiperSlide className={style.swiperSlide}>
         <div className={style.title}>
