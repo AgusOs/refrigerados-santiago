2a1b975c1d9060b7a4c30e5f123405a9fa5db6d0
AgusOs <osorioagus96@gmail.com>
2025-08-03 09:36:18 -0300

    animando portada con motion

--- src/components/pages/empresa/EmpresaPage.jsx
+++ src/components/pages/empresa/EmpresaPage.jsx
@@ -1,18 +1,36 @@
 import React from 'react'
 import styles from './EmpresaPage.module.css';
-import { Slide } from '@mui/material';
+import { motion, useInView } from 'motion/react';
 
 export const EmpresaPage = () => {
 
   return (
     <div className={styles.container}>
-      <Slide direction="left" in={true} timeout={1000} mountOnEnter unmountOnExit>
-        <div className={styles.portada}>
-          <img src="\assets\images\local.webp" alt="portada1" />
-          <img src="\assets\images\empresa-portada2.webp" alt="portada2" />
-        </div>
-      </Slide>
-      <div className={styles.misionvisionvalores}>
+      <motion.div 
+      className={styles.portada}
+      initial={{ x: 100, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{ x: 100, opacity: 0 }}
+      transition={{
+        type: "spring",
+        stiffness: 65,
+        damping: 10
+      }}
+      >
+        <img src="\assets\images\local.webp" alt="portada1" />
+        <img src="\assets\images\empresa-portada2.webp" alt="portada2" />
+      </motion.div>
+      <motion.div 
+      className={styles.misionvisionvalores}
+      initial={{ x: 100, opacity: 0 }}
+      whileInView={{ x: 0, opacity: 1 }}
+      viewport={{ once: true, amount: 0.3 }} 
+      transition={{
+        type: "spring",
+        stiffness: 65,
+        damping: 10
+      }}
+      >
         <div className={styles.cardTexto}>
           <div className={styles.cardContent}>
             <h3 className={styles.cardTitle}>MISIÓN</h3>
@@ -57,15 +75,25 @@ export const EmpresaPage = () => {
             </p>
           </div>
         </div>
-      </div>
-      <div className={styles.content}>
+      </motion.div>
+      <motion.div 
+      className={styles.content}
+      initial={{ x: 100, opacity: 0 }}
+      whileInView={{ x: 0, opacity: 1 }}
+      viewport={{ once: true, amount: 0.3 }} 
+      transition={{
+        type: "spring",
+        stiffness: 65,
+        damping: 10
+      }}
+      >
         <h4>Santiago nace hace más de 60 años en La Pampa como un emprendimiento familiar dedicado a la distribución de alimentos refrigerados.</h4>
         <h4>Desde el inicio, el compromiso fue claro: hacer las cosas bien, con responsabilidad, cercanía y respeto por cada cliente.</h4>
         <h5>Hoy somos una empresa mayorista de alimentos y bebidas consolidada, con una estructura de 13 sucursales distribuidas a lo largo de La Pampa, tecnología adaptada, logística propia y un equipo de trabajo excepcional con personas que comparten los mismos valores de siempre. <br />
           Adiscionalmente, distribuimos a locales gastronómicos y de cercanía en toda la provincia, garantizando calidad, atención personalizada y entregas a tiempo. Nuestro objetivo es ayudar y apoyar al comercio pampeano. <br />
           Sabemos que detrás de cada pedido hay una gran responsabilidad, y por eso trabajamos cada día para acompañar, facilitar y potenciar el crecimiento de quienes nos eligen.  
         </h5>
-      </div>
+      </motion.div>
       <div className={styles.imagenes}>
         <img src="\assets\galeria\1.JPG" alt="empresa1" />
         <img src="\assets\galeria\2.JPG" alt="empresa2" />
